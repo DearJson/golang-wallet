b929b68cd2f6a30604b9eba5d36f43165fafa13c
dsf <admin@qq.com>
2026-02-26 22:02:53 +0800

    update .gitignore

---
+++ cmd/generate_address.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	// 生成私钥
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 获取私钥字节
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	privateKeyHex := hexutil.Encode(privateKeyBytes)[2:] // 移除0x前缀
+
+	// 从私钥获取公钥
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("无法转换公钥")
+	}
+
+	// 生成地址
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+
+	// 输出结果
+	fmt.Println("BSC地址生成成功:")
+	fmt.Println("地址:", address)
+	fmt.Println("私钥:", privateKeyHex)
+	fmt.Println("\n注意: 请妥善保管私钥，不要泄露给任何人！")
+}
